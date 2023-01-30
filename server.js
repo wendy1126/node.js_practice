5b970735d930d9300eb89c1499f2a1ed0ef69a25
wendy1126 <kbbb9278@naver.com>
2023-01-30 13:33:36 +0900

    mongoDB 연

--- server.js
+++ server.js
@@ -3,11 +3,25 @@ const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const MongoClient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient; //DB접속(connect)하기 위해 작성
 
+var db; //어떤 폴더에 DB를 저장할 것인지 명시 필요, 변수 하나 필요
 MongoClient.connect(
-  'mongodb+srv://kbb9278:<qhqo1126%21>@cluster0.z4k54lg.mongodb.net/todoapp?retryWrites=true&w=majority',
+  //DB Access 메뉴에서 만든 아이디, 비번 입력필요(비번 특수문자는 변환 필요)
+  //connect는 연결해주세요고, function은 연결되면 실행할 내용들
+  'mongodb+srv://kbb9278:qhqo1126%21@cluster0.z4k54lg.mongodb.net/todoapp?retryWrites=true&w=majority',
   function (에러, client) {
+    if (에러) return console.log(에러); //'에러' 파라미터는, 에러 발생 시 어떤 에러인지 알려주는 역할 파라미터
+
+    db = client.db('todoapp'); //todoapp이라는 DB(폴더)에 연결 해주세요
+
+    db.collection('post').insertOne(
+      { 이름: 'john', 나이: 20 },
+      function (에러, 결과) {
+        console.log('저장완료');
+      }
+    );
+
     app.listen(8080, function () {
       console.log('listening on 8080');
     });
