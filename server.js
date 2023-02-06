331d4ccb58f51c6b80d48c0073e3bea6c0fd02f0
WENDY <97227279+wendy1126@users.noreply.github.com>
2023-02-06 18:07:46 +0900

    로그인 기능 / 환경변수 파일생성

--- server.js
+++ server.js
@@ -6,6 +6,17 @@ const MongoClient = require("mongodb").MongoClient; //mongoDB 접속하기 위�
 app.set("view engin", "ejs"); //ejs 사용하기 위해 작성
 const methodOverride = require("method-override"); //put과 delete 요청할 수 있는 라이브러리 method-override 쓰기 위해 작성
 app.use(methodOverride("_method")); //put과 delete 요청할 수 있는 라이브러리 method-override 쓰기 위해 작성
+const passport = require("passport"); //session방식 로그인 기능 구현하기 위한 라이브러리1
+const LocalStrategy = require("passport-local").Strategy; //session방식 로그인 기능 구현하기 위한 라이브러리2
+const session = require("express-session"); //session방식 로그인 기능 구현하기 위한 라이브러리3
+require("dotenv").config(); //환경변수 사용을 위한 라이브러리
+
+//app.user(미들웨어) : 웹서버는 요청-응답해주는 머신이기때문에 중간에 실행되는 코드를 미들웨어라고 함
+app.use(
+  session({ secret: "비밀코드", resave: true, saveUninitialized: false })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use("/public", express.static("public")); //내가 작성한 css 파일 첨부하기 위해 작성
 
@@ -13,7 +24,7 @@ var db;
 //mongoDB connect에서 복붙 (아이디,비밀번호,프로젝트이름 확인 필수:비번에 특수문자는 변환필요)
 MongoClient.connect(
   //접속해주세요라고 요청하는 곳이고
-  "mongodb+srv://wendy1126:godnjs1126@cluster0.pvfkvnu.mongodb.net/?retryWrites=true&w=majority",
+  process.env.DB_URL,
   function (에러, client) {
     //연결되면 할 일
     if (에러) return console.log(에러);
@@ -26,7 +37,7 @@ MongoClient.connect(
     //   }
     // );
 
-    app.listen(8080, function () {
+    app.listen(process.env.PORT, function () {
       console.log("listening on 8080");
     });
   }
@@ -155,4 +166,82 @@ app.put("/edit", function (요청, 응답) {
       응답.redirect("/list");
     }
   );
+});
+
+//로그인 페이지 접속
+app.get("/login", function (요청, 응답) {
+  응답.render("login.ejs");
+});
+
+//서버가 로그인 요청 시...
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/fail",
+  }),
+  function (요청, 응답) {
+    //로그인 성공 시 처리하는 코드
+    응답.redirect("/");
+  }
+);
+
+//마이페이지 만들기
+app.get("/mypage", 로그인했니, function (요청, 응답) {
+  console.log(요청.user);
+  응답.render("mypage.ejs", { 사용자: 요청.user });
+});
+
+//mypage 접속 전 실행할 미들웨어
+function 로그인했니(요청, 응답, next) {
+  if (요청.user) {
+    next();
+  } else {
+    응답.send("로그인 안했습니다.");
+  }
+}
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "id",
+      passwordField: "pw",
+      session: true, //세션을 저장할 것인지
+      passReqToCallback: false,
+    },
+    //아이디/비번 맞는 지 DB와 비교 검증하는 부분
+    function (입력한아이디, 입력한비번, done) {
+      console.log(입력한아이디, 입력한비번);
+      db.collection("login").findOne(
+        { id: 입력한아이디 },
+        function (에러, 결과) {
+          if (에러) return done(에러);
+          //밑에부터 아주 중요
+          //done은 3개의 파라미터를 가질 수 있음 : done(서버에러, 성공시 사용자 DB데이터, 에러메세지)
+          if (!결과)
+            return done(null, false, { message: "존재하지않는 아이디요" });
+          if (입력한비번 == 결과.pw) {
+            return done(null, 결과); //'결과'는 아이디 비번 검증 성공시 아래의 user에 들어감
+          } else {
+            return done(null, false, { message: "비번틀렸어요" });
+          }
+        }
+      );
+    }
+  )
+);
+
+// 세션만들기, 로그인 성공->세션정보 만듦->마이페이지 방문시 세션검사
+// user.id를 이용해서 세션을 저장시키는 코드(로그인 성공시 발동)
+passport.serializeUser(function (user, done) {
+  //검증 코드의 '결과'가 user에 저장돔
+  done(null, user.id); //세션 데이터를 만들고 세션의 id 정보를 쿠키로 보냄
+});
+
+//로그인한 유저의 세션아이디를 바탕으로 개인정보를 DB에서 찾는 역할(마이페이지 접속시 발동)
+passport.deserializeUser(function (아이디, done) {
+  //위 코드의 user.id가 '아이디'와 동일
+  //db에서 위에 있는 user.id로 유저를 찾은 뒤에 유저 정보를 중괄호 안에 넣음(user의 DB를 마이페이지에서 출력해주려고)
+  db.collection("login").findOne({ id: 아이디 }, function (에러, 결과) {
+    done(null, 결과);
+  });
 });
