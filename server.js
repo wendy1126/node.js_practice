d6bb4f2d5e7deedd9399c5a3a1f6ad2b39ca4ef9
WENDY <97227279+wendy1126@users.noreply.github.com>
2023-02-06 10:48:24 +0900

    게시물 ID 달기

--- server.js
+++ server.js
@@ -62,10 +62,33 @@ app.post("/add", function (요청req, 응답res) {
   응답res.send("전송완료");
   // console.log(요청req.body.title);
   // console.log(요청req.body.date);
-  db.collection("post").insertOne(
-    { 제목: 요청req.body.title, 날짜: 요청req.body.date }, //원하는 데이터
-    function () {
-      console.log("저장완료");
+  db.collection("counter").findOne(
+    { name: "게시물갯수" },
+    function (에러, 결과) {
+      console.log(결과.totalPost);
+      var 총게시물갯수 = 결과.totalPost;
+
+      db.collection("post").insertOne(
+        {
+          _id: 총게시물갯수 + 1,
+          제목: 요청req.body.title,
+          날짜: 요청req.body.date,
+        }, //원하는 데이터
+        function () {
+          console.log("저장완료");
+          //counter라는 콜렉션에 있는 totalPost라는 항목도 1 증가시켜야함(수정)
+          //db.collection('counter').updateOne({어떤데이터를 수정할지},{$연산자:{수정값}},function(){})
+          db.collection("counter").updateOne(
+            { name: "게시물갯수" },
+            { $inc: { totalPost: 1 } },
+            function (에러, 결과) {
+              if (에러) {
+                return console.log(에러);
+              }
+            }
+          );
+        }
+      );
     }
   );
 });
